222bdbf1d183a4faa4cbc7cf9bf6380060c6e8c2
rombot9000 <rombot9000@users.noreply.github.com>
2020-02-08 14:30:26 +0100

    Adjusted search bar
    - esc clears search
    - backspace is handled sperately for faster search

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -25,6 +25,34 @@ function SearchBar(props) {
 
     const classes = useStyles();
     const inputRef = React.useRef(null);
+    const [inputValue, setInputValue] = React.useState("")
+
+    /**
+     * Special key press handling
+     * @param {KeyboardEvent} event 
+     */
+    function handleKeyDown(event) {
+        if(event.key === "Escape") {
+            event.preventDefault();
+            setInputValue("");
+            MopidyHandler.filterAlbums("");
+        } else if(event.key === "Backspace") {
+            // Is faster than onInput-Callback!
+            event.preventDefault();
+            setInputValue(inputValue.slice(0,-1));
+            MopidyHandler.filterAlbums(inputValue.slice(0,-1));
+        }
+    }
+
+    /**
+     * Is slower than onKeyDown :-/
+     * @param {InputEvent} event 
+     */
+    function handleInput(event) {
+        event.preventDefault();
+        MopidyHandler.filterAlbums(event.target.value);
+        setInputValue(event.target.value);
+    }
     
     return (
         <Container
@@ -42,8 +70,10 @@ function SearchBar(props) {
                     className={classes.input}
                     inputRef={inputRef}
                     disableUnderline={true}
-                    onInput={e => MopidyHandler.filterAlbums(e.target.value)}
+                    value={inputValue}
                     placeholder="Search for Albums or Artists..."
+                    onInput={handleInput}
+                    onKeyDown={handleKeyDown}
                 />
                 <IconButton onClick={() => inputRef.current.focus()}>
                     <Search fontSize="inherit"/>
