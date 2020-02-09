853995e51daa795fa5954692865330b538355485
rombot9000 <rombot9000@users.noreply.github.com>
2020-02-09 10:37:32 +0100

    Added custom error classes

---
+++ src/MopidyHandler/Errors.js
@@ -0,0 +1,22 @@
+export class UnknownPlaybackCmdError extends Error {
+    /**
+     * @param {string} cmd 
+     */
+    constructor(cmd) {
+        super(`Unknown Playback state: ${cmd}`);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+
+export class UnknownPlaybackStateError extends Error {
+    /**
+     * @param {string} state 
+     */
+    constructor(state) {
+        super(`Unknown Playback state: ${state}`);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
