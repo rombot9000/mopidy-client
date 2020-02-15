fb99c4751d6968511150a5ddd4957b5a93d3eb33
rombot9000 <rombot9000@users.noreply.github.com>
2020-02-15 21:26:21 +0100

    New component SquareImage:
    - can be used to display images using a 1:1 aspect ratio
    - uses the background-image hack

---
+++ src/Components/SquareImage.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { makeStyles, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        width: "100%",
+        paddingTop: "100%",
+    }
+}));
+
+/**
+ * @typedef {Object} SquareImagePropType
+ * @property {string} src
+ * 
+ * @typedef {SquareImagePropType & import("@material-ui/core").PaperProps} SquareImageProps
+ */
+
+/**
+ * 
+ * @param {SquareImageProps} props 
+ */
+function SquareImage(props) {
+
+    const {to, src, ...paperProps} = props;
+
+    const classes = useStyles();
+
+    return (
+        <Paper className={classes.paper} {...paperProps}
+            style={{
+                backgroundImage: `url(${src})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "100% 100%",
+            }}>
+        </Paper>
+    );
+}
+
+export default SquareImage;
