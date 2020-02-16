4c0ba993cfde7d67e02f103af59035ad5a3f2b34
rombot9000 <rombot9000@users.noreply.github.com>
2020-02-16 11:05:25 +0100

    Moved main view from App.js to sparated component

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
 import React from "react";
 
-import { Box } from '@material-ui/core';
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
+
 import MopidyHandler from "MopidyHandler/MopidyHandler";
+import MainView from "Components/MainView";
 
-import AlbumGrid from "Components/AlbumGrid";
-import AlbumDetails from "Components/AlbumDetails";
-import ScollableModal from "Components/ScrollableModal";
-import PlaybackCtrlBar from "Components/PlaybackCtrlBar";
-import SearchBar from "Components/SearchBar";
-import MenuDrawer from "Components/MenuDrawer";
-
-const useStyles = makeStyles(theme => ({
-    rootBox: {
-        width: '100%',
-        height: '100%',
-    },
-    searchBar: {
-        zIndex: 1100,
-        position: "fixed",
-        width: "100%",
-        maxWidth: theme.spacing(64),
-        padding: theme.spacing(1,1,0,1),//top-right-bottom-left
-        left: "50%",
-        transform: "translate(-50%,0)",
-    }
-}));
 
 
 function App() {
-    const classes = useStyles();
-
     const [state, setState] = React.useState({
         mpdState: null,
-    });
-
-    const [view, setView] = React.useState({
-        height: 0,
-        paddingTop: 0
-    });
-
-    const[showMenuDrawer, setShowMenuDrawer] = React.useState(false);
-
-    const [components, setComponents] = React.useState({
-        /** @type {Object.<string, JSX.Element>} */
-        "components": {}
     });
 
     // setup listener
@@ -57,58 +22,13 @@ function App() {
         };
     },[]); // only execute once
 
-    // adjust offset of view depending on ctrl bar height
-    const ctrlBarRef = React.useRef(null);
-    const srchBarRef = React.useRef(null);
-    React.useEffect(() => {
-        const height = ctrlBarRef.current ? ctrlBarRef.current.offsetHeight : 0;
-        const paddingTop = srchBarRef.current ? srchBarRef.current.offsetHeight : 0;
-        setView({
-            height: height,
-            paddingTop: paddingTop
-        });
-    }, [ctrlBarRef, srchBarRef]); // listen for ctrl bar changes
-
-
-    /**
-     * @param {import('ViewModel/Album').Album} album 
-     */
-    function openDetailsModal(album) {
-        function closeDetailsModal() {
-            delete components.components["detailsModal"];
-            setComponents(components);
-        }
-        components.components["detailsModal"] = (
-            <ScollableModal key="detailsModal" open onClose={closeDetailsModal}>
-                <AlbumDetails album={album}/>
-            </ScollableModal>
-        );
-        setComponents({
-            "components": components.components
-        });
-    }
-
-    function toggleSideMenu() {
-        setShowMenuDrawer(!showMenuDrawer);
-    }
-
     return (
         <React.Fragment>
             <CssBaseline/>
-            <SearchBar className={classes.searchBar} onMenuIconClick={toggleSideMenu} ref={srchBarRef}/>
-            <MenuDrawer open={showMenuDrawer} onClose={toggleSideMenu}/>
-            <Box 
-                className={classes.rootBox}
-                marginBottom={`${view.height}px`}
-                paddingTop={`${view.paddingTop}px`}
-            >
-                <AlbumGrid
-                    albums={MopidyHandler.Albums}
-                    onTileClick={openDetailsModal}
-                />
-            </Box>
-            <PlaybackCtrlBar ref={ctrlBarRef}/>
-            {Object.values(components.components)}
+            <MainView
+                state={state.mpdState}
+                albums={MopidyHandler.Albums}
+            />
         </React.Fragment>
     );
 };
