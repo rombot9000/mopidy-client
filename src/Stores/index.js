5b6e48b738358f04dcf53ff18942dccf9df62e63
rombot9000 <rombot9000@users.noreply.github.com>
2020-05-01 15:46:45 +0200

    Redux integration complete, removed stores

--- src/Stores/index.js
+++
@@ -1,6 +0,0 @@
-import Dispatcher from "Dispatcher";
-
-import playbackStore from "./playbackStore";
-
-export const PlaybackStore = new playbackStore();
-Dispatcher.register(PlaybackStore.handleAction.bind(PlaybackStore));
