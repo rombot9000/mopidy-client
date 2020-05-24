103d35a68882b4ca2a55c84efbd8b9fb31be35b2
rombot9000 <rombot9000@users.noreply.github.com>
2020-05-24 15:08:35 +0200

    Adjusted component for album tracklist

--- src/Components/AlbumTrack.js
+++ src/Components/AlbumTrack.js
@@ -1,25 +1,18 @@
 import React from "react";
 
-import { SvgIcon, Grid, Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { PlayArrow, Pause } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core";
 
-/**
- * Animated eq icon
- * @param {import("@material-ui/core").SvgIconProps} props 
- */
-const AnimatedEq = (props) => (
-    <SvgIcon {...props} viewBox="0 0 24 24">
-        <line x1="3" x2="3" y1="24" y2="0" strokeWidth="6" stroke="black">
-            <animate attributeName="y2" values="0;6;12;18;24;0" dur="1s" repeatCount="indefinite" />
-        </line>
-        <line x1="12" x2="12" y1="24" y2="0" strokeWidth="6" stroke="black">
-            <animate attributeName="y2" values="18;24;0;6;12;18" dur="1s" repeatCount="indefinite" />
-        </line>
-        <line x1="21" x2="21" y1="24" y2="0" strokeWidth="6" stroke="black">
-            <animate attributeName="y2" values="6;12;18;24;0;6" dur="1s" repeatCount="indefinite" />
-        </line>
-    </SvgIcon>
-)
+import AnimatedEq from "./AnimatedEq";
+
+import useFormatedTime from "Hooks/useFormatedTime";
+
+const useStyles = makeStyles({
+    text: {
+        fontWeight: props => props.playbackState === "stopped" ? "normal" : 500
+    }
+});
 
 /**
  * 
@@ -28,49 +21,23 @@ const AnimatedEq = (props) => (
  * @param {import("Reducers/PlaybackReducer").MopdiyPlaybackState} props.playbackState
  * @param {number} props.playbackTimePosition
  */
-export default ({track, playbackState, playbackTimePosition, ...gridProps}) => {
+export default ({track, playbackState, playbackTimePosition, dispatch, ...gridProps}) => {
+
+    const classes = useStyles({playbackState});
 
     const firstCellReducer = (firstCell, action) => {
         switch(action) {
-            case "TRACK_NO": return track.track_no;
-            case "EQ": return <AnimatedEq fontSize="inherit"/>;
-            case "PLAY": return <PlayArrow fontSize="inherit"/>;
-            case "PAUSE": return <Pause fontSize="inherit"/>;
             case "CTRL": return playbackState === "playing" ? <Pause fontSize="inherit"/> : <PlayArrow fontSize="inherit"/>;
             case "INFO": return playbackState === "stopped" ?  track.track_no : <AnimatedEq fontSize="inherit"/>;
             default: return firstCell;
         }
     };
-    const [firstCell, setFirstCell] = React.useReducer(firstCellReducer, track.track_no);
+    const [firstCell, setFirstCell] = React.useReducer(
+        firstCellReducer,
+        playbackState === "stopped" ?  track.track_no : <AnimatedEq fontSize="inherit"/>
+    );
 
-    const secondsReducer = (seconds, action) => {
-        switch(action.type){
-            case "SET_MS": return Math.floor(action.value/1000);
-            case "INCREMENT": return seconds + 1; 
-            default: return seconds;
-        }
-    };
-    const [seconds, setSeconds] = React.useReducer(secondsReducer, Math.floor(track.length/1000));
-
-    // set last cell content (duration or time position)
-    React.useEffect(() => {
-        
-        if(playbackState === "paused") {
-
-            setFirstCell("EQ");
-            setSeconds({type: "SET_MS", value: playbackTimePosition});
-
-        } else if(playbackState === "playing") {
-
-            setFirstCell("EQ");
-            setSeconds({type: "SET_MS", value: playbackTimePosition});
-
-            let interval = setInterval(() => setSeconds({type: "INCREMENT"}), 1000);
-            return () => clearInterval(interval);
-            
-        }
-
-    }, [playbackState, playbackTimePosition, setSeconds]);
+    const timeString = useFormatedTime(track.length, playbackState, playbackTimePosition);
 
     return (
         <Grid
@@ -82,13 +49,13 @@ export default ({track, playbackState, playbackTimePosition, ...gridProps}) => {
             spacing={2}
         >
             <Grid item xs={1}>
-                <Typography variant="body2" align="right">{firstCell}</Typography>
+                <Typography className={classes.text} variant="body1" align="right">{firstCell}</Typography>
             </Grid>
             <Grid item xs={9}>
-                <Typography variant="body2" align="left">{track.name}</Typography>
+                <Typography className={classes.text} variant="body1" align="left">{track.name}</Typography>
             </Grid>
             <Grid item xs={2}>
-                <Typography variant="body2" align="right">{Math.floor(seconds/60)}:{`00${seconds%60}`.slice(-2)}</Typography>
+                <Typography className={classes.text} variant="body1" align="right">{timeString}</Typography>
             </Grid>
         </Grid>
     );
---
+++ src/Components/AnimatedEq.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { SvgIcon } from "@material-ui/core";
+
+/**
+ * Animated eq icon
+ * @param {import("@material-ui/core").SvgIconProps} props 
+ */
+export default (props) => (
+    <SvgIcon {...props} viewBox="0 0 24 24">
+        <line x1="3" x2="3" y1="24" y2="0" strokeWidth="6" stroke="black">
+            <animate attributeName="y2" values="0;6;12;18;24;0" dur="1s" repeatCount="indefinite" />
+        </line>
+        <line x1="12" x2="12" y1="24" y2="0" strokeWidth="6" stroke="black">
+            <animate attributeName="y2" values="18;24;0;6;12;18" dur="1s" repeatCount="indefinite" />
+        </line>
+        <line x1="21" x2="21" y1="24" y2="0" strokeWidth="6" stroke="black">
+            <animate attributeName="y2" values="6;12;18;24;0;6" dur="1s" repeatCount="indefinite" />
+        </line>
+    </SvgIcon>
+)
