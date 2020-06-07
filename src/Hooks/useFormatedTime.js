860cbb6a34f5abd35d6de70fa928b3f5f144f256
rombot9000 <rombot9000@users.noreply.github.com>
2020-06-07 20:13:17 +0200

    Fixd bug in useFormatedTime hook

--- src/Hooks/useFormatedTime.js
+++ src/Hooks/useFormatedTime.js
@@ -6,22 +6,39 @@ import React from "react";
  * @param {number} playbackTimePosition current time position in mesecs
  */
 export default (length, playbackState, playbackTimePosition) => {
-
-    const [seconds, incrementSeconds] = React.useReducer(
-        seconds => seconds+1,
+    
+    const [seconds, setSeconds] = React.useReducer(
+        (seconds, action) => {
+            switch(action.type) {
+                case "setMillisecs": return Math.floor(action.value/1000);
+                case "increment": return seconds + 1;
+                default: return seconds;
+            }
+        },
         Math.floor((playbackState === "stopped" ? length/1000: playbackTimePosition/1000))
     );
 
     React.useEffect(() => {
         
-       if(playbackState === "playing") {
+        switch(playbackState) {
 
-            const interval = setInterval(incrementSeconds, 1000);
-            return () => clearInterval(interval);
+            case "stopped":
+                setSeconds({type: "setMillisecs", value: length});
+                return;
+
+            case "paused":
+                setSeconds({type: "setMillisecs", value: playbackTimePosition});
+                return;
             
+            case "playing":
+                setSeconds({type: "setMillisecs", value: playbackTimePosition});
+                const interval = setInterval(() => setSeconds({type: "increment"}), 1000);
+                return () => clearInterval(interval);
+
+            default: console.warn("Unknonw playback state:", playbackState);
         }
 
-    }, [playbackState, playbackTimePosition]);
+    }, [length, playbackState, playbackTimePosition]);
 
     return `${Math.floor(seconds/60)}:${`00${seconds%60}`.slice(-2)}`;
 };
