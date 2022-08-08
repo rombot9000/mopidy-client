3cd2020e323bfb292fd7b5d515a6657e08966fe7
rombot9000 <rombot9000@users.noreply.github.com>
2022-08-08 18:13:07 +0200

    Create index file for hooks

---
+++ src/Hooks/index.js
@@ -0,0 +1,13 @@
+import useFormatedTime from "./useFormatedTime";
+import useHeight from "./useHeight";
+import useIntersection from "./useIntersection";
+import useScrollToIfActive from "./useScrollToIfActive";
+import useTrackIcon from "./useTrackIcon";
+
+export {
+    useFormatedTime,
+    useHeight,
+    useIntersection,
+    useScrollToIfActive,
+    useTrackIcon,
+}
