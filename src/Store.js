13bcfeab10b8b41527ae5d1511f00c0592b000d4
rombot9000 <rombot9000@users.noreply.github.com>
2022-08-11 09:33:21 +0200

    Add logger middleware to store

--- src/Store.js
+++ src/Store.js
@@ -1,3 +1,4 @@
+// @preval
 import { configureStore } from '@reduxjs/toolkit'
 
 import libraryReducer from "./Reducers/LibraryReducer";
@@ -6,6 +7,24 @@ import notifyReducer from "./Reducers/NotifyReducer";
 import viewReducer from "./Reducers/ViewReducer";
 import tracklistReducer from "./Reducers/TracklistReducer";
 import playbackReducer from "./Reducers/PlaybackReducer";
+
+
+/**
+ * Logs all actions and states after they are dispatched.
+ */
+ const logger = store => next => action => {
+    console.group(`${action.type} - ${action.case}`);
+    console.info('dispatching', action);
+    let result = next(action);
+    console.log('next state', store.getState());
+    console.groupEnd();
+    return result;
+}
+
+const middleware = [];
+if(process.env.NODE_ENV !== "production") {
+    middleware.push(logger);
+}
 
 export default configureStore({ 
     reducer: {
@@ -16,5 +35,6 @@ export default configureStore({
         tracklist: tracklistReducer,
         playback: playbackReducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleware),
     devTools: process.env.NODE_ENV !== 'production',
 })
