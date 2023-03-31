a43dd67ddc0ca4aec43392e18a2d6e826803b0ed
rombot9000 <rombot9000@users.noreply.github.com>
2023-03-31 17:36:49 +0200

    Added long press hook

--- src/Hooks/index.js
+++ src/Hooks/index.js
@@ -4,7 +4,8 @@ import useIntersection from "./useIntersection";
 import useScrollToIfActive from "./useScrollToIfActive";
 import useTrackIcon from "./useTrackIcon";
 import useTimeString from "./useTimeString";
-import useReadableString from "./useReadableString"
+import useReadableString from "./useReadableString";
+import useLongPress from "./useLongPress";
 
 export {
     useFormatedTime,
@@ -14,4 +15,5 @@ export {
     useTrackIcon,
     useTimeString,
     useReadableString,
+    useLongPress
 }
